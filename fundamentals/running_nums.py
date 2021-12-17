27395ffeceee21b913394b6c12faf8d4131ddc6a
Amy M Haddad <amy.m.haddad@gmail.com>
2021-12-17 08:13:04 -0500

    Adding all solutions to function problems

--- fundamentals/running_nums.py
+++ fundamentals/running_nums.py
@@ -1,19 +1,20 @@
+#Sol 1
 def running_sums(nums):
-    prev = nums[0]
     new_list = len(nums) * [None]
-    for i in range(1, len(nums)-1, 1):
-        curr = nums[i]+ nums[i-1]
-        new_list[i] = curr
-        prev = curr
+    prev = nums[0]
+    new_list[0] = prev
+
+    for i in range(1, len(nums), 1):
+        prev += nums[i]
+        new_list[i] = prev
     return new_list
-    # new_list = len(nums) * [None]
-    # prev = nums[0]
-    # new_list[0] = prev
-    #
-    # for i in range(1, len(nums), 1):
-    #
-    #     prev += nums[i]
-    #     new_list[i] = prev
-    # return new_list
-    
-print(running_sums([5, 4, 2, 1, 5, 6, 4]))
+
+#Sol 2
+def running_sums(nums):
+    new_nums = []
+    total = 0
+
+    for num in nums:
+        total += num 
+        new_nums.append(total)
+    return new_nums
