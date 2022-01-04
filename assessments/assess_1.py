48bc06c6e66343f3b29cd52933bea980dde7c903
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-04 10:37:51 -0500

    Added 2nd solution

--- assessments/assess_1.py
+++ assessments/assess_1.py
@@ -1,5 +1,6 @@
 import random
 
+#Sol 1
 def user_numbers():
     start = 0
     end = 0
@@ -48,4 +49,43 @@ def guess_number():
         print(f"You guessed the number in {attempts} attempts")
     else:
         print(f"You guessed the number in {attempts} attempt")
-guess_number()
+#guess_number()
+
+#Sol 2
+import random 
+
+INVALID_INPUT = 'Please enter a valid number.'
+def num_range():
+    start = input("Enter the start of the range: ")
+
+    while not start.isdigit():
+        print(INVALID_INPUT)
+        start = input("Enter the start of the range: ")
+
+    end = input("Enter the end of the range: ")
+    while not end.isdigit() or int(end) < int(start):
+        print(INVALID_INPUT)
+        end = input("Enter the end of the range: ")
+
+    return int(start), int(end)
+
+def guesser():
+    attempts = 0
+    guess = None
+    start, end = num_range()
+    random_num = random.randint(start, end)
+    
+    while random_num != guess:
+        guessed_num = input("Guess a number: ")
+
+        if not guessed_num.isdigit():
+            print(INVALID_INPUT)
+            continue
+        attempts += 1
+        guess = int(guessed_num)
+    
+    if attempts > 1:
+        print(f"You guessed the number in {attempts} attempts")
+    else:
+        print(f"You guessed the number in {attempts} attempt")
+guesser()
