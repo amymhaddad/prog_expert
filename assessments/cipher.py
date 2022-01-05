f95877787320aaabb090a6248a35f99866ac82fd
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-05 15:46:06 -0500

    Added second solution :

--- assessments/cipher.py
+++ assessments/cipher.py
@@ -1,21 +1,29 @@
 """
 https://www.programmingexpert.io/programming-fundamentals/assessment/2
 """
-
+#Sol 1
 from string import ascii_lowercase as lower 
 
 ALPHA_LENGTH = 26
 
-def caesar_cipher(string, offset):
-    
-    new_word = ""
+def caesar_cipher(string, offset):  
+    new_word = []
     letter_indexes = {i: letter for i, letter in enumerate(lower)} 
     
     for char in string:
-        index = ord(char) - ord("a")
-        letter_index = letter_indexes[index]
-        new_char = (index - offset) % 26
-        new_word += letter_indexes[new_char]
-    return new_word
+        dist = ord(char) - ord("a")
+        index = (dist - offset) % 26
+        new_word.append(letter_indexes[index])
+    return "".join(new_word)
 
-#print(caesar_cipher("apple", 5))
+#Sol 2 
+def caesar_cipher(string, offset):
+    
+    new_word = []
+    alpha  =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    for char in string:
+        curr_index = alpha.index(char)
+        index = curr_index - offset 
+        new_word.append(alpha[index])
+    return "".join(new_word)
