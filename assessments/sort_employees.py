5c5edade7b1df11da81613387245bd9ceb3d4318
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-06 14:37:49 -0500

    First commit

---
+++ assessments/sort_employees.py
@@ -0,0 +1,13 @@
+"""
+https://www.programmingexpert.io/programming-fundamentals/assessment/3
+"""
+
+EMPLOYEE_CATEGORIES = {"name": 0, "age": 1, "salary": 2}
+
+people = [["john", 33, 65000]]
+
+for person in people:
+    if 33 in person:
+        print("here")
+
+
