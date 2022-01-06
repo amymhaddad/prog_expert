e8ebf09c40fdcb216a8fd1054c4816c120d57d97
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-06 17:16:11 -0500

    Added 2nd solution

--- assessments/sort_employees.py
+++ assessments/sort_employees.py
@@ -5,25 +5,27 @@ https://www.programmingexpert.io/programming-fundamentals/assessment/3
 import operator
 
 employees = [["John", 33, 65000], ["Sarah", 24, 75000], ["Josie", 29, 1000000], ["Jason", 26, 55000], ["Connor", 25, 110000]]
+categories = {"name": 0, "age": 1, "salary": 2}
+
+
+
+def v0_sort_employees(employees, sort_by):
+print(v0_sort_employees(employees, "age"))
+        
+
 
 def sort_employees(employees, sort_by):
     index = categories[sort_by]
 
-    sortd_values = sorted(employees, key=operator.itemgetter(index))
-
-
-    print(sortd_values)  
-
-
-print(sort_employees(employees, "age"))
+    return sorted(employees, key=operator.itemgetter(index))
 
 
 
 
-# people = [["john", 33, 65000]]
-#
-# for person in people:
-#     if 33 in person:
-#         print("here")
-#
-#
+def v2_sort(employees, sort_by):
+    index = categories[sort_by]
+
+    return sorted(employees, key=lambda x: x[index])
+
+
+
