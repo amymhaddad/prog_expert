9749c9547a5021770d433d356b5fc33e80953c35
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-09 07:35:36 -0500

    Refactored solution

--- assessments/longest_unique_words.py
+++ assessments/longest_unique_words.py
@@ -4,6 +4,13 @@ https://www.programmingexpert.io/programming-fundamentals/assessment/4
 
 
 def get_n_longest_unique_words(words, n):
+    unique_words = get_unique_words(words)
+    sorted_words = sorted(unique_words.items(), key=lambda x: x[1], reverse=True)
+
+    return [word[0] for word in sorted_words[:n]]
+
+
+def get_unique_words(words):
 
     unique_words = {}
 
@@ -12,10 +19,7 @@ def get_n_longest_unique_words(words, n):
             unique_words.pop(word)
         else:
             unique_words[word] = len(word)
-    sorted_words = sorted(unique_words.items(), key=lambda x: x[1], reverse=True)
-
-    return [word[0] for word in sorted_words[:n]]
-
+    return unique_words
 
 words = ["Longer", "Whatever", "Longer", "Ball", "Rock", "Rocky", "Rocky"]
 print(get_n_longest_unique_words(words, 3))
