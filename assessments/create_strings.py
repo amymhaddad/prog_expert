3e8176dd2b006e5d58413d807de02a44b4b1d122
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-11 17:18:29 -0500

    Solved it

--- assessments/create_strings.py
+++ assessments/create_strings.py
@@ -2,34 +2,34 @@
 https://www.programmingexpert.io/programming-fundamentals/assessment/6
 """
 
-def create_strings_from_characters(frequencies, string2):
 
-   # import pdb; pdb.set_trace()
-   # string1_val = is_valid(frequencies, string1) 
+def create_strings_from_characters(frequencies, string1, string2):
+    string1_val = is_valid(frequencies, string1)
     string2_val = is_valid(frequencies, string2)
 
-    
-    print(string2_val)
-
+    if string1_val and string2_val:
+        return 2
+    elif string1_val or string2_val:
+        return 1
+    else:
+        return 0
 
 
 def is_valid(frequencies, string):
-
-    new_string = None
+    valid_string = 1
+    prev = ""
     for letter in string:
         count = string.count(letter)
-        
-        if letter not in frequencies or frequencies[letter] < count or frequencies[letter] == 0:
-            print("here")
-            new_string = 0 
-        else:
-            frequencies[letter] -= count 
+        if letter == prev:
+            continue
+        elif (
+            letter not in frequencies
+            or frequencies[letter] < count
+            or frequencies[letter] == 0
+        ):
+            valid_string = 0
+            break
 
-    if isinstance(new_string, type(None)):
-        new_string = 1
-
-    return new_string
-frequencies = {"h": 2, "e": 1, "l": 1, "r": 4, "a": 3, "o": 2, "d": 1, "w": 1}
-string1 = "hello"
-string2 = "world"
-print(create_strings_from_characters(frequencies, string2))
+        frequencies[letter] -= count
+        prev = letter
+    return valid_string
--- assessments/test.py
+++ assessments/test.py
@@ -1,9 +1,17 @@
 import pytest
 
 from cipher import caesar_cipher
+from create_strings import create_strings_from_characters
 
 
 def test_cipher():
     assert caesar_cipher("hello", 3) == "ebiil"
     assert caesar_cipher("apple", 5) == "vkkgz"
     assert caesar_cipher("tim", 2) == "rgk"
+
+
+def test_create_strings():
+    frequencies = {"a": 3, "b": 5, "c": 3, "d": 2, "e": 1}
+    string1 = "aaabbbc"
+    string2 = "bbccde"
+    assert create_strings_from_characters(frequencies, string1, string2) == 2
