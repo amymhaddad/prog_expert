886dbef0b56cd55e84a16095d7651d3bf17029ed
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-11 17:39:22 -0500

    Adding second solution

--- assessments/create_strings.py
+++ assessments/create_strings.py
@@ -2,34 +2,68 @@
 https://www.programmingexpert.io/programming-fundamentals/assessment/6
 """
 
+#Sol 1
+# def create_strings_from_characters(frequencies, string1, string2):
+#     can_create_string1 = is_valid_string(frequencies, string1)
+#     can_create_string2 = is_valid_string(frequencies, string2)
+#
+#     if can_create_string1 and can_create_string2:
+#         return 2
+#     elif can_create_string1 or can_create_string2:
+#         return 1
+#   return 0
+#
+#
+# def is_valid_string(frequencies, string):
+#     valid_string = 1
+#     prev = ""
+#     for letter in string:
+#         count = string.count(letter)
+#         if letter == prev:
+#             continue
+#         elif (
+#             letter not in frequencies
+#             or frequencies[letter] < count
+#             or frequencies[letter] == 0
+#         ):
+#             valid_string = 0
+#             break
+#
+#         frequencies[letter] -= count
+#         prev = letter
+#     return valid_string
+#
 
+#Sol 2
+# def create_strings_from_characters(frequencies, string1, string2):
+#     can_create_string1 = is_valid_string(frequencies, string1)
+#     can_create_string2 = is_valid_string(frequencies, string2)
+
+#     if can_create_string1 and can_create_string2:
+#         return 2
+#     elif can_create_string1 or can_create_string2:
+#         return 1
+#     return 0
+
+# def is_valid_string(frequencies, string):
+#     for letter in set(string):
+#         if letter not in frequencies or string.count(letter) > frequencies[letter] or frequencies[letter] == 0:
+#             return 0
+#         frequencies[letter] -= string.count(letter)
+#     return 1
+
+#Sol 3
 def create_strings_from_characters(frequencies, string1, string2):
-    string1_val = is_valid(frequencies, string1)
-    string2_val = is_valid(frequencies, string2)
+    can_create_string1 = is_valid_string(frequencies, string1)
+    can_create_string2 = is_valid_string(frequencies, string2)
 
-    if string1_val and string2_val:
+    if can_create_string1 and can_create_string2:
         return 2
-    elif string1_val or string2_val:
+    elif can_create_string1 or can_create_string2:
         return 1
-    else:
-        return 0
+    return 0
 
-
-def is_valid(frequencies, string):
-    valid_string = 1
-    prev = ""
-    for letter in string:
-        count = string.count(letter)
-        if letter == prev:
-            continue
-        elif (
-            letter not in frequencies
-            or frequencies[letter] < count
-            or frequencies[letter] == 0
-        ):
-            valid_string = 0
-            break
-
-        frequencies[letter] -= count
-        prev = letter
-    return valid_string
+frequencies = {"h": 2, "e": 1, "l": 2, "r": 4, "a": 3, "o": 2, "d": 1, "w": 1}
+string1 = "hello"
+string2 = "world"
+print(create_strings_from_characters(frequencies, string1, string2))
