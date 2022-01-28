f702635adf565e13d29234c9a5b8a8d26de94967
Amy M Haddad <amy.m.haddad@gmail.com>
2022-01-28 14:31:32 -0500

    mend

--- oop/animal.py
+++ oop/animal.py
@@ -4,26 +4,30 @@ class Animal:
         self.weight = weight
         self.height = height
 
+
 class Mammal(Animal):
     def __init__(self, age, weight, height, sex):
         super().__init__(age, weight, height)
-        self.sex = sex 
-     
+        self.sex = sex
+
 
 class Reptile(Animal):
     def __init__(self, age, weight, height, legs):
         super().__init__(age, weight, height)
         self.legs = legs
 
+
 class Monkey(Mammal):
     fingers = 5
+
     def __init__(self, age, weight, height, sex, color):
         super().__init__(age, weight, height, sex)
-        self.color = color 
-        
+        self.color = color
+
 
 class Lizard(Reptile):
-    tail = True 
+    tail = True
+
     def __init__(self, age, weight, height, legs, color):
         super().__init__(age, weight, height, legs)
-        self.color = color 
+        self.color = color
