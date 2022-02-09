5b32cf3488bd92dc73ddca962fccf005bf31ff97
Amy M Haddad <amy.m.haddad@gmail.com>
2022-02-09 13:17:29 -0500

    Added max_stocked_item

--- assessments/oop_assessments/inventory.py
+++ assessments/oop_assessments/inventory.py
@@ -8,9 +8,10 @@ class Inventory:
     def add_item(self, name, price, quantity):
 
       # inventory_quantity = sum([item[0] for item in self.total_items])
+        #import pdb; pdb.set_trace()
         if name in self.total_items or self.curr_capacity + quantity > self.max_capacity:
             return False
-        else:
+        else:   
             self.total_items[name] = [price, quantity]
             self.curr_capacity += quantity
             return True    
@@ -18,22 +19,36 @@ class Inventory:
     def delete_item(self, name):
         if name not in self.total_items:
             return False
+        
+        #import pdb; pdb.set_trace()
+        self.curr_capacity -= self.total_items[name][1]
         del self.total_items[name]
         return True
 
     def get_items_in_price_range(self, min_price, max_price):
-        # Write your code here.
-        pass
+        items_in_price_range = []
+        for key, val in self.total_items.items():
+            if val[0] >= min_price and val[0] <= max_price:
+                items_in_price_range.append(key)
+        return items_in_price_range
 
     def get_most_stocked_item(self):
-        
-        max_stocked_item = max(quantity[1]    for quantity in self.total_items.values())
 
+        #Check for none values and check if items have the same amount -- ie, quantities are both 2. In this case, I can't use max
+        import pdb; pdb.set_trace()
+        max_stocked_item = max([quantity[1]    for quantity in self.total_items.values()], default=0)
 
-item1 = Inventory(50)
-item1.add_item("item1", 5, 2)
-item1.add_item("item2", 5, 3)
-print(item1.get_most_stocked_item())
-# item1.add_item("item3", 5, 2)
+        for key, val in self.total_items.items():
+            if val[1] == max_stocked_item:
+                return key
 
+inventory = Inventory(4)
+inventory.add_item('Chocolate', 4.99, 4)
+inventory.delete_item('Chocolate')
+inventory.delete_item('Chocolate')
+inventory.delete_item('Bread')
+inventory.add_item('Chocolate', 4.99, 2)
+inventory.add_item('Bread', 4.99, 2)
 
+#print(inventory.total_items) 
+print(inventory.get_most_stocked_item())
