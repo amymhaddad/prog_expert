abe96e8ffa5275f5e65ff458ed34ccde8eb7d2aa
Amy M Haddad <amy.m.haddad@gmail.com>
2022-02-11 13:54:04 -0500

    Updated getter/setter

--- assessments/oop_assessments/student.py
+++ assessments/oop_assessments/student.py
@@ -1,30 +1,22 @@
-from functools import total_ordering
-import imp
-
 
 class Student:
     all_students = []
 
     def __init__(self, name, grade):
         self.name = name 
+        self._grade = grade
+        Student.all_students.append(self)
+        
+    @property
+    def grade(self):
+        return self._grade 
+
+    @grade.setter
+    def grade(self, grade):
         if grade < 0 or grade > 100:
             raise ValueError("Invalid grade")
         else:
-            import pdb; pdb.set_trace()
             self.grade = grade 
-        Student.all_students.append(self)
-
-    @property
-    def grade(self):
-        return self.grade 
-
-    # @grade.setter
-    # def grade(self):
-    #    # import pdb; pdb.set_trace()
-    #     if self.grade < 0 or self.grade > 100:
-    #         raise ValueError("Invalid grade")
-    #     else:
-    #         self.grade = self.grade 
 
     def calculate_average_grade(students):
         if len(students) == 0:
@@ -42,7 +34,7 @@ class Student:
         total_grade = 0 
         for student in cls.all_students:
             total_grade += student.grade 
-        return total_grade / len(cls.all_students)
+        return int(total_grade / len(cls.all_students))
 
 
     @classmethod
@@ -56,12 +48,11 @@ class Student:
             if student.grade > best_grade:
                 best_grade = student.grade 
                 best_student = student.name 
-        return best_student
+        return Student(best_student, best_grade)
         
 
 student1 = Student("Antoine", 75)
-#average = Student.get_average_grade()
-#best_student = Student.get_best_student()
-print(student1)
-
+average = Student.get_average_grade()
+best_student = Student.get_best_student()
+print(best_student.name)
     
